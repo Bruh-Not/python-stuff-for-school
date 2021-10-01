c80a432263f1f235a3d588619057d7efe3c3e0eb
Bruh-Not <91141513+Bruh-Not@users.noreply.github.com>
2021-10-01 12:42:28 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,4 @@
-def add(x,y):
-    return x+y
-    
-def subtract(x,y):
-    return x-y
-    
-def multiply(x,y):
-    return x*y
-    
-def divide(x,y):
-    return x/y
+from math import *
     
 print('Addition (+)')
 print('Subtraction (-)')
