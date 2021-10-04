aa1ff6855202372361d456601ad3cef37903a771
Bruh-Not <91141513+Bruh-Not@users.noreply.github.com>
2021-10-04 10:48:09 -0600

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -24,19 +24,19 @@ while True:
         num2 = float(input('Second number: '))
         
         if choice == '+':
-            print(num1, '+', num2, '=', add(num1, num2))
+            print(num1, 'plus', num2, 'is', add(num1, num2))
             
         elif choice == '-':
-            print(num1, '-', num2, '=', subtract(num1, num2))
+            print(num1, 'subtracted by', num2, 'is', subtract(num1, num2))
             
         elif choice == '*':
-            print(num1, '*', num2, '=', multiply(num1, num2))
+            print(num1, 'multiplied by', num2, 'is', multiply(num1, num2))
             
         elif choice == '/':
-            print(num1, '/', num2, '=', divide(num1, num2))
+            print(num1, 'divided by', num2, 'is', divide(num1, num2))
             
         elif choice == '^':
-            print(num1, '^', num2, '=', pow(num1, num2))
+            print(num1, 'to the power of', num2, 'is', pow(num1, num2))
             
         next_calculation = input('Do another one? [y/n]: ')
         if next_calculation == 'n':
